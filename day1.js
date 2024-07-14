62a4d4f3897015fc7a3ce1f1dd75c0c0c54251d0
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-07-14 23:43:37 +0530

    task 1 completed

---
+++ day1.js
@@ -0,0 +1,6 @@
+// task 1: Declare a variable var, assign it a number, and log the value to the console.
+
+var num = 10;
+console.log(num);
+
+// task 2: Declare a variable using let, assign it a string, and log the value to the console.
