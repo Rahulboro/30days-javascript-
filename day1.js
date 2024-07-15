4b24ebed243907d0017353d9ac9b52afb4be828d
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-07-15 23:56:07 +0530

    update task 4" Create a variable of different data types (number, string boolean, object, array) and log each variable's type using the typeof operator"

--- day1.js
+++ day1.js
@@ -1,17 +1,27 @@
-//Activity 1: Variable Declaration
+// ***          Activity 1: Variable Declaration            ***
 
-// task 1: Declare a variable var, assign it a number, and log the value to the console.
-
+// TASK 1: Declare a variable var, assign it a number, and log the value to the console.
 var num = 10;
 console.log(num);
 
-// task 2: Declare a variable using let, assign it a string, and log the value to the console.
-
+// TASK 2: Declare a variable using let, assign it a string, and log the value to the console.
 let str = "Hello ujjal ";
 console.log(str);
 
-// Activity 2: Constant Declaration
+// ***          Activity 2: Constant Declaration            ***
 
-// task 3: Declare a variable using const,  assign it a boolean value, and log the value to the console
+// TASK 3: Declare a variable using const,  assign it a boolean value, and log the value to the console
 const bool = true;
 console.log(bool);
+
+// TASK 4: Create a variable of different data types (number, string boolean, object, array) and log each variable's type using the typeof operator
+let oneTaskNum = 10;
+let oneTaskStr = "Hello Ujjal";
+let oneTaskBoolean = true;
+let oneTaskObj = { name: "ujjal Moni Bordoloi", Age: 25 };
+let oneTaskArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(typeof oneTaskNum);
+console.log(typeof oneTaskStr);
+console.log(typeof oneTaskBoolean);
+console.log(typeof oneTaskObj);
+console.log(typeof oneTaskArr);
