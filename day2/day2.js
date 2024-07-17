73f6763f94a7d3833508f70de8534d23d51c3619
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-07-18 00:56:03 +0530

    "Added code for tasks 2-4, implementing subtraction, multiplication, and division operations."

--- day2/day2.js
+++ day2/day2.js
@@ -6,7 +6,19 @@ let taskOneResult = taskOneNum + taskOneNum2;
 console.log(taskOneResult);
 
 //Task 2: Write a program to subtract two numbers and log the result to the console.
+let taskTwoNum = 50;
+let taskTwoNum2 = 20;
+let taskTwoResult = taskTwoNum - taskTwoNum2;
+console.log(taskTwoResult);
 
 //Task 3: Write a program to multiply two numbers and log the result to the console.
+let taskThreeNum = 10;
+let taskThreeNum2 = 20;
+let taskThreeResult = taskThreeNum * taskThreeNum2;
+console.log(taskThreeResult);
 //Task 4: Write a program to divide two numbers and log the result to the console.
+let taskFourNum = 100;
+let taskFourNum2 = 20;
+let taskFourResult = taskFourNum / taskFourNum2;
+console.log(taskFourResult);
 //Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
