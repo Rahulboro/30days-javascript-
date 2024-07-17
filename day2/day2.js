522631485728677718064c72eb09f12780cef692
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-07-18 00:50:59 +0530

    day 2 Task 1  Writing a program to add two numbers and log the result to the console. Completed

---
+++ day2/day2.js
@@ -0,0 +1,12 @@
+//***               Activity 1: Arithmetic Operations               ***
+//Task 1: Write a program to add two numbers and log the result to the console.
+let taskOneNum = 10;
+let taskOneNum2 = 20;
+let taskOneResult = taskOneNum + taskOneNum2;
+console.log(taskOneResult);
+
+//Task 2: Write a program to subtract two numbers and log the result to the console.
+
+//Task 3: Write a program to multiply two numbers and log the result to the console.
+//Task 4: Write a program to divide two numbers and log the result to the console.
+//Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
